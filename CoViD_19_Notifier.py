9e53ce8b74d27bdd546eaa88e6797fa9583711ea
Yashasvi Bhatt <yashasvibhatt9@gmail.com>
2020-09-22 15:15:15 +0530

    CoViD-19 Notifier

---
+++ CoViD_19_Notifier.py
@@ -0,0 +1,71 @@
+# Importing the libraries
+
+from plyer import notification
+import requests
+from bs4 import BeautifulSoup
+from babel.numbers import format_number
+import datetime
+import csv
+
+
+def notifyMe(title, message):
+    '''function to give notification'''
+    notification.notify(title = title, message = message, app_icon = 'images/covid19.ico', timeout = 10)
+
+def getData(url):
+    '''function to fetch data from url'''
+    r = requests.get(url)                                       # fetch data from url
+    return r.text
+
+if __name__ == '__main__':
+    # web scrapping
+    data = getData('https://www.mohfw.gov.in/')
+    soup = BeautifulSoup(data, 'html.parser')                   # parsing html data
+    # print(soup.prettify())                                      # prettify() kept the original form of data
+
+    data = soup.find_all('strong')                              # fetching all strong tag data
+    # data = ''.join(data)
+    mystr = ''
+    for dta in data:                                            # conversion to string
+        mystr += str(dta)
+        mystr += '\n'
+    soup = BeautifulSoup(mystr, 'html.parser')                  # passing string to parse as html document
+    # print(soup)
+    data = soup.find_all('strong', {'class': 'mob-hide'})       # finding all strong with mob-hide class
+    mystr = ''
+    for dta in data:                                            # converting fetched data to string
+        mystr += str(dta)
+        mystr += '\n'
+    soup = BeautifulSoup(mystr, 'html.parser')                  # passing string to parse as html document
+    data = soup.find_all('strong')
+    mystr = ''
+    for dt in data:                                             # fetching the data inside the strong tag
+        mystr += dt.get_text()
+    mylist = mystr.split('\xa0')                                # creating a list of fetched data
+    mylist = ' '.join(mylist).split()                           # removing spaces from strings in list
+
+    total_cases = int(mylist[1]) + int(mylist[3]) + int(mylist[5])
+    total_cases = format_number(total_cases, locale='en_IN')
+
+    active_cases = int(mylist[1])
+    active_cases = format_number(active_cases, locale='en_IN')
+
+    recovered_cases = int(mylist[3])
+    recovered_cases = format_number(recovered_cases, locale='en_IN')
+
+    demised_cases = int(mylist[5])
+    demised_cases = format_number(demised_cases, locale='en_IN')
+
+    notifyMe('CoViD-19 Status of India', f'Total Reported Cases : {total_cases}\nRecovered Cases : {recovered_cases}\nActive Cases : {active_cases}\nDeaths : {demised_cases}')
+
+    date = str(datetime.datetime.now().date())
+    time = str(datetime.datetime.now().hour) + ':' + str(datetime.datetime.now().minute)
+
+    # writing fetched data to file
+
+    data_row = [date, time, total_cases, recovered_cases, active_cases, demised_cases]
+
+    file = 'data.csv'
+    with open(file, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)                     # creating a csv writer object
+        csvwriter.writerow(data_row)                        # writing data in row
---
+++ README.md
@@ -0,0 +1,25 @@
+# CoViD-19 Notifier
+
+## To run this Project please follow these steps
+
+### A. Pre-Requisites
+1. Open _command prompt_ or _powershell window_.
+2. Type this command<br>`git clone https://github.com/YashasviBhatt/TKinter-Projects`<br>and press enter.
+3. Go inside the _Cloned Repository_ folder and open _command-prompt_ or _powershell window_.
+
+### B. Executing the Project
+#### Recommended Method
+1. Make sure the location where your _terminal_ is open should be inside the _Cloned repository_ Folder.
+2. Type<br>`pip install virtualenv`<br>and press enter.
+3. Now, type<br>`.\covid_19_notifier\Script\activate`<br>and press enter.
+    - if you are having Error while _activating virtual environment_ then open _command prompt_ or _powershell window_ as _administrator_.
+    - now type<br>`set-executionpolicy remotesigned`<br>press enter and repeat _step 3_.
+4. After activating _virtual environment_, the _path_ should look like this ```(covid_19_notifier) .\<your-path>\CoViD-19_Notifier```.
+5. Run the Project using this<br>`python CoViD_19_Notifier.py`.
+6. If you would like to see the _data_ which showed up then open **data.csv** file.
+
+#### Alternate Method
+In case you don't want to use _virtual environment_ which was has all the required _libraries_ installed then follow these steps:<br>
+1. Make sure the location where your _terminal_ is open should be inside the _Cloned repository_ Folder.
+2. Type<br>`pip install -r requirements.txt`<br> and press enter in either _command_prompt_ or _powershell window_ as _administrator_.
+3. After Installing all the required _libraries_ execute the program<br>`python CoViD_19_Notifier.py`.
